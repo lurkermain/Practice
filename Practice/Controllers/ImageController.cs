37fc5589d078f8eccae4aea2ac78930288ec8bc8
killpathhh <jenya.sofronoff@gmail.com>
2025-01-16 21:11:45 +0300

    1

--- Practice/Controllers/ImageController.cs
+++ Practice/Controllers/ImageController.cs
@@ -17,72 +17,6 @@ namespace Practice.Controllers
             _context = context;
         }
 
-        /*[HttpGet("products/{id}/rendered-image")]
-        public IActionResult GetRenderedImage(int id)
-        {
-            //  изображения сохраняются в папке "C:\render"
-            string imagePath = Path.Combine(@"C:\blender_render", $"left_{id}.png");
-
-            if (!System.IO.File.Exists(imagePath))
-            {
-                return NotFound($"Rendered image for product {id} not found.");
-            }
-
-            byte[] imageBytes = System.IO.File.ReadAllBytes(imagePath);
-            return File(imageBytes, "image/png");
-        }*/
-
-        /*[HttpPost("products/{id}/render")]
-        public async Task<IActionResult> RenderProductImage(int id, [FromQuery] float angle, [FromQuery] float light)
-        {
-            var product = await _context.Products.FindAsync(id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-
-            string blenderExe = "\"X:\\BlenderFoundation\\Blender4.3\\blender.exe\"";
-            string scriptPath = "\"X:\\BlenderFoundation\\Blender4.3\\script2.py\"";
-            string filepath = $@"C:\blender_render\rendered_image_{id}.png";
-            string modelpath = @"C:\blender_fruto\front_fruto_nyanya_half.gltf";
-            string texturepath = @"C:\blender_fruto\front 1 bunny.png";
-
-            string arguments = $"--background --python \"{scriptPath}\" -- --filepath \"{filepath}\" --modelpath \"{modelpath}\" --texturepath \"{texturepath}\" --angle {angle} --light {light}";
-
-
-            var processStartInfo = new ProcessStartInfo
-            {
-                FileName = blenderExe,
-                Arguments = arguments,
-                RedirectStandardOutput = true,
-                RedirectStandardError = true,
-                UseShellExecute = false,
-                CreateNoWindow = true,
-            };
-
-            processStartInfo.Verb = "runas";
-
-
-            // log
-            Console.WriteLine($"Blender Path: {blenderExe}");
-            Console.WriteLine($"Arguments: {arguments}");
-
-
-            using var process = new Process { StartInfo = processStartInfo };
-            process.Start();
-
-            string output = await process.StandardOutput.ReadToEndAsync();
-            string error = await process.StandardError.ReadToEndAsync();
-            process.WaitForExit();
-
-            if (process.ExitCode != 0)
-            {
-                return StatusCode(500, $"Error rendering image: {error}");
-            }
-
-            return Ok($"Image for product {id} rendered successfully.");
-        }*/
-
         [HttpPost("products/render")]
         public IActionResult Render(int angle, int lightEnergy, IFormFile file)
         {
@@ -150,6 +84,74 @@ namespace Practice.Controllers
                 return StatusCode(500, new { error = $"Ошибка при выполнении рендеринга: {ex.Message}" });
             }
         }
+
+        /*[HttpGet("products/{id}/rendered-image")]
+        public IActionResult GetRenderedImage(int id)
+        {
+            //  изображения сохраняются в папке "C:\render"
+            string imagePath = Path.Combine(@"C:\blender_render", $"left_{id}.png");
+
+            if (!System.IO.File.Exists(imagePath))
+            {
+                return NotFound($"Rendered image for product {id} not found.");
+            }
+
+            byte[] imageBytes = System.IO.File.ReadAllBytes(imagePath);
+            return File(imageBytes, "image/png");
+        }*/
+
+        /*[HttpPost("products/{id}/render")]
+        public async Task<IActionResult> RenderProductImage(int id, [FromQuery] float angle, [FromQuery] float light)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            string blenderExe = "\"X:\\BlenderFoundation\\Blender4.3\\blender.exe\"";
+            string scriptPath = "\"X:\\BlenderFoundation\\Blender4.3\\script2.py\"";
+            string filepath = $@"C:\blender_render\rendered_image_{id}.png";
+            string modelpath = @"C:\blender_fruto\front_fruto_nyanya_half.gltf";
+            string texturepath = @"C:\blender_fruto\front 1 bunny.png";
+
+            string arguments = $"--background --python \"{scriptPath}\" -- --filepath \"{filepath}\" --modelpath \"{modelpath}\" --texturepath \"{texturepath}\" --angle {angle} --light {light}";
+
+
+            var processStartInfo = new ProcessStartInfo
+            {
+                FileName = blenderExe,
+                Arguments = arguments,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            };
+
+            processStartInfo.Verb = "runas";
+
+
+            // log
+            Console.WriteLine($"Blender Path: {blenderExe}");
+            Console.WriteLine($"Arguments: {arguments}");
+
+
+            using var process = new Process { StartInfo = processStartInfo };
+            process.Start();
+
+            string output = await process.StandardOutput.ReadToEndAsync();
+            string error = await process.StandardError.ReadToEndAsync();
+            process.WaitForExit();
+
+            if (process.ExitCode != 0)
+            {
+                return StatusCode(500, $"Error rendering image: {error}");
+            }
+
+            return Ok($"Image for product {id} rendered successfully.");
+        }*/
+
+
 
 
     }
