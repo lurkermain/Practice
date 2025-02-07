8ec4277c95c9574eba3ea88e79e51597f88c4f15
killpathhh <jenya.sofronoff@gmail.com>
2025-02-07 21:16:46 +0300

    ehseker

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -33,7 +33,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:5173");
+        policy.WithOrigins("https://musical-cactus-08f1b2.netlify.app");
         policy.AllowAnyHeader();
         policy.AllowAnyMethod();
     });
